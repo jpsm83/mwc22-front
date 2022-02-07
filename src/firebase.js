b1c141d3e7bd175bb80a7a5994925e81e39d7a99
jpsm83 <jpsm83@hotmail.com>
2022-02-07 10:24:55 +0100

    client server folders

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAlTV3vHfMK2Q3tmINn6Dr66wr8kMg8ENU",
-    authDomain: "mwc22-front.firebaseapp.com",
-    projectId: "mwc22-front",
-    storageBucket: "mwc22-front.appspot.com",
-    messagingSenderId: "977573406720",
-    appId: "1:977573406720:web:6bf1afa6a1b0814eb450e5"
-  };
